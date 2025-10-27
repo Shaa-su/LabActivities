32642fcd76cfb4e9e51a7f25d60e1951b3a15621
Shaasu <2022100854@ms.bulsu.edu.ph>
2025-10-27 16:34:54 +0800

    Activity 2 done

--- Python/Activity 2/Lab2_Maglalang_DonErrole.py
+++ Python/Activity 2/Lab2_Maglalang_DonErrole.py
@@ -3,28 +3,7 @@ products = {
     "P002": {"name": "Smartphone", "price": 15000},
     "P003": {"name": "Headphones", "price": 2000},
     "P004": {"name": "USB Drive", "price": 500},
-}
-
-# === Calculations ===
-def compute_subtotal(cart):
-    #Compute subtotal from all items in the cart
-    return sum(item["total"] for item in cart)
-
-def compute_discount(subtotal):
-    #Apply 10% discount if subtotal >= 20000
-    if subtotal >= 20000:
-        return subtotal * 0.10
-    return 0
-
-def compute_tax(subtotal, discount):
-    #Compute 12% VAT after discount
-    return (subtotal - discount) * 0.12
-
-def compute_total(subtotal, discount, tax):
-    #Compute final total
-    return subtotal - discount + tax
-
-
+};
 # === MAIN PROGRAM ===
 print("=== Welcome to BulSU E-Commerce Store ===")
 print("")
@@ -65,6 +44,25 @@ while True:
         print(f"X {e}\n")
     except ValueError as e:
         print(f"X {e}\n")
+
+        # === Calculations ===
+def compute_subtotal(cart):
+    #Compute subtotal from all items in the cart
+    return sum(item["total"] for item in cart)
+
+def compute_discount(subtotal):
+    #Apply 10% discount if subtotal >= 20000
+    if subtotal >= 20000:
+        return subtotal * 0.10
+    return 0
+
+def compute_tax(subtotal, discount):
+    #Compute 12% VAT after discount
+    return (subtotal - discount) * 0.12
+
+def compute_total(subtotal, discount, tax):
+    #Compute final total
+    return subtotal - discount + tax
 
 # === CHECKOUT ===
 print("\n=== OFFICIAL RECEIPT ===")
